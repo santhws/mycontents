38815713e942f3146fced29417f010fcc5abd973
Santiago <santiagodossantos332@gmail.com>
2025-04-09 19:47:05 -0300

    Adicionando Strings no Console

--- Work-Space/script.js
+++ Work-Space/script.js
@@ -1,1 +1,22 @@
-alert("Rola-Preta");
+// // alert("Hello-Word");
+// var caixa;
+
+// caixa = "lego";
+
+// console.log(caixa);
+
+// ---------------------------------------------------
+
+var idade = 20;
+
+var menor10 = idade <= 10;
+var maior30 = idade >= 30;
+
+var gratuidade = menor10 || maior30;
+
+console.log("Idade", idade);
+console.log("Menor que 10", menor10);
+console.log("Maior que 30", maior30);
+console.log("Tem direito a gratuidade?", gratuidade);
+
+// ---------------------------------------------------
