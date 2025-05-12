a74100becbf1d616fbe15112edd74a2ab5e71d09
Santiago <santiagodossantos332@gmail.com>
2025-05-12 20:29:22 -0300

    Adiconando uma nova instância do banco de dados

--- Python-e-Flask/app.py
+++ Python-e-Flask/app.py
@@ -1,20 +1,23 @@
 # Importação 
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config('SQLALCHEMY_DATABASE_URI') = 'sqlite:///ecormmerce.db' #configuração do banco de dados
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ecommerce.db' #configuração do banco de dados
 
 db = SQLAlchemy(app) #conexão do banco de dados
 
 #Modelagem
+
 class Product(db.Model):
-    id = db.Collumn(db.Interger, primary_key=True)
-    name = db.Collumn(db.String(120), nullable=False)
-    price = db.Collumn(db.Float, nullable=False)
-    description = db.Collunm(db.Text, nullable=True)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    description = db.Column(db.Text, nullable=True)
 
 # Definir uma rota raiz (página inicial) e a função que será executada ao requisitar
+
 @app.route('/')
 def Hello_Word():
     return 'Hello World'
